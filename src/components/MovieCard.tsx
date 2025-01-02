01cd314adb73d200b6311f3d766cd537ef8f72ae
Paulina <paulina.a.gaweda@gmail.com>
2025-01-02 15:43:15 +0100

    Use mui components

--- src/components/MovieCard.tsx
+++ src/components/MovieCard.tsx
@@ -1,4 +1,13 @@
+import {
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  Rating,
+  Typography,
+} from "@mui/material";
 import { Movie } from "../models/MovieModel";
+import { Check, Close } from "@mui/icons-material";
 
 interface MovieCardProps {
   movie: Movie;
@@ -8,15 +17,29 @@ export const MovieCard = ({
   movie: { title, rating, imageURL, summary },
 }: MovieCardProps) => {
   return (
-    <div>
-      <h1>{title}</h1>
-      <p>Rating: {rating}/10</p>
-      <img src={imageURL} alt={title} />
-      <div>{summary}</div>
-      <div>
-        <button>Accept</button>
-        <button>Reject</button>
-      </div>
-    </div>
+    <Card sx={{ maxWidth: 345, paddingBottom: 5 }}>
+      <CardContent>
+        <h2>{title}</h2>
+        <Typography component="legend">Rating: {rating}/10</Typography>
+        <Rating
+          name="read-only"
+          value={rating}
+          precision={0.5}
+          size="small"
+          max={10}
+          readOnly
+        />
+        <img src={imageURL} alt={title} />
+        <div>{summary}</div>
+      </CardContent>
+      <CardActions sx={{ justifyContent: "center" }}>
+        <Button variant="contained" color="success" startIcon={<Check />}>
+          Accept
+        </Button>
+        <Button variant="contained" color="error" startIcon={<Close />}>
+          Reject
+        </Button>
+      </CardActions>
+    </Card>
   );
 };
